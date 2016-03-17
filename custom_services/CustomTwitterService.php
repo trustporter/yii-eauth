b8b96e2a57e2023d5406d0bdd9bf9116a2745f78
Aggelos Giantsios <aggelgian@gmail.com>
2016-03-18 00:24:24 +0200

    Add Twitter

--- custom_services/CustomTwitterService.php
+++ custom_services/CustomTwitterService.php
@@ -23,4 +23,62 @@ class CustomTwitterService extends TwitterOAuthService {
 		$this->attributes['timezone'] = timezone_name_from_abbr('', $info->utc_offset, date('I'));
 		$this->attributes['photo'] = $info->profile_image_url;
 	}
+
+  public function getInterestingInfo($fields = "")
+  {
+    $info = (array) $this->makeSignedRequest('https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true');
+    Yii::log(CVarDumper::dumpAsString($info));
+    return $info;
+  }
+
+  /**
+   * Override method and do not store the token in the session
+   */
+  protected function saveAccessToken($token)
+  {
+    $this->access_token = $token->access_token;
+  }
+
+  /**
+   * Returns the social ID of the user, if connected.
+   *
+   * @return array the result.
+   */
+  public function getUserId()
+  {
+    try
+    {
+      $info = (array) $this->makeSignedRequest("https://api.twitter.com/1.1/account/verify_credentials.json");
+      return array('connected' => true, 'id' => $info['id']);
+    }
+    catch (EAuthException $e)
+    {
+      return array('connected' => false);
+    }
+  }
+
+
+  /**
+   * Gets a list of friends and for each gets the requested fields.
+   *
+   * @param string $fields what you want to fetch.
+   * @return array the list of friends.
+   */
+  public function getAllMyFriends($userId)
+  {
+    $friends = array();
+    $cursor = -1;
+    $limit = 200;
+    while ($cursor != 0)
+    {
+      $data = (array) $this->makeSignedRequest("https://api.twitter.com/1.1/friends/list.json?cursor=$cursor&user_id=$userId");
+      //Yii::log(CVarDumper::dumpAsString($data));
+      $cursor = (int) $data["next_cursor"];
+      $friends = array_merge($friends, $data["users"]);
+    }
+    return array_map(function($of) {
+        $f = (array) $of;
+        return $f;
+      }, $friends);
+  }
 }
